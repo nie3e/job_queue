e44c92833f645f72016e9cfc11e887acb63a3d90
nie3e <adrcwiek@gmail.com>
2026-05-17 17:36:00 +0200

    Updates init

--- src/job_queue/__init__.py
+++ src/job_queue/__init__.py
@@ -1,7 +1,9 @@
 from job_queue.service_layer.worker import JobHandler, WorkerService
+from job_queue.domain.model import Job
 
 
 __all__ = [
     "JobHandler",
-    "WorkerService"
+    "WorkerService",
+    "Job"
 ]
