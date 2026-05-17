c85c40720bd07325dc0342b98f276138f7343550
nie3e <adrcwiek@gmail.com>
2026-05-17 17:54:49 +0200

    Fixes init and setup

--- src/job_queue/__init__.py
+++ src/job_queue/__init__.py
@@ -1,9 +1,2 @@
 from job_queue.service_layer.worker import JobHandler, WorkerService
 from job_queue.domain.model import Job
-
-
-__all__ = [
-    "JobHandler",
-    "WorkerService",
-    "Job"
-]
--- src/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="job_queue",
-    version="0.0.1",
-    packages=["job_queue"]
-)
